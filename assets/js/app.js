b1587cb89187bb4d05611afa0b2f0fe3b4855a6b
Eduardo Landeros <ls.eduardo.11@gmail.com>
2025-05-26 11:18:29 -0600

    cambios en en los enlaces de html y en la funcion promesa del js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name');//cambiar la llamada a la clase name
+const $b = document.querySelector('.blog');
+const $l = document.querySelector('.location'); // agregar un elemento con la clase location
 
-function displayUser(username) {
+async function displayUser(username) { // en esta linea se agrega la expresion para tener una promesa
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  const response = await fetch(`${usersEndpoint}/${username}`); // la expresion await solo es permitida en funciones async
   console.log(data);
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
@@ -16,7 +16,7 @@ function displayUser(username) {
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`// correccion de la referencia a la variable $n
 }
 
 displayUser('stolinski').catch(handleError);
